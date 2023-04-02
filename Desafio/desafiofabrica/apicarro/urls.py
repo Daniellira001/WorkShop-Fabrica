0b4faf756ca4edf3d172d7533553c3b1bce79576
Daniellira001 <danielliradeandrade01@gmail.com>
2023-04-01 23:45:51 -0300

    DesafioPronto

--- Desafio/desafiofabrica/apicarro/urls.py
+++ Desafio/desafiofabrica/apicarro/urls.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from carro.views import UsuarioViewSet, CarroUsoViwSet
-from rest_framework import routers
+from carro.views import UsuarioViewSet, CarroUsoViwSet, CarroUsodelete, Usuariodelete
 
-router = routers.DefaultRouter()
-router.register(r'Usuario', UsuarioViewSet)
-router.register(r'CarroUso', CarroUsoViwSet)
+
+
 
 urlpatterns = [
-    path('CarroUso/', include(router.urls)),
-    path('Usuario/', include(router.urls)),
+    path('CarroUso/', CarroUsoViwSet.as_view()),
+    path('Usuario/', UsuarioViewSet.as_view()),
     path('admin/', admin.site.urls),
+    path('CarroUso/<int:pk>/', CarroUsodelete.as_view()),
+    path('Usuario/<int:pk>/', Usuariodelete.as_view()),
     path('api-auth/', include('rest_framework.urls'))
 ]
--- Desafio/desafiofabrica/carro/views.py
+++ Desafio/desafiofabrica/carro/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import generics
 from .models import Usuario, CarroUso
 from .serializers import UsuarioSerializer, CarroUsoSerializer
 
 
-class UsuarioViewSet(viewsets.ModelViewSet):
+class UsuarioViewSet(generics.ListCreateAPIView):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
 
-class CarroUsoViwSet(viewsets.ModelViewSet):
+class Usuariodelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+class CarroUsoViwSet(generics.ListCreateAPIView):
+    queryset = CarroUso.objects.all()
+    serializer_class = CarroUsoSerializer
+
+class CarroUsodelete(generics.RetrieveUpdateDestroyAPIView):
     queryset = CarroUso.objects.all()
     serializer_class = CarroUsoSerializer
 
